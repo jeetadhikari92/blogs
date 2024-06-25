518f6a5b07ced711202d5d797c44bcac203357d9
jeetadhikari92 <jeet.adhikari92@gmail.com>
2024-06-25 15:50:11 +0200

    Added Sign in frontend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -105,7 +105,7 @@ export const SignUp = () => {
           </form>
           <div className="flex gap-2 text-small mt-5">
             <span>Have an account?</span>
-            <Link to="sign-in" className="text-blue-500">
+            <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
           </div>
