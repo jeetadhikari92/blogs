cff3a5ab9fd98a240a8f8fc39e561804e4c40794
jeetadhikari92 <jeet.adhikari92@gmail.com>
2024-06-26 01:54:31 +0200

    dark mode theme

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,16 @@
 import React from "react"
-import {
-  Avatar,
-  Button,
-  Dropdown,
-  DropdownDivider,
-  DropdownItem,
-  Navbar,
-  TextInput,
-} from "flowbite-react"
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
 import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch } from "react-icons/ai"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { toggleTheme } from "../redux/theme/themeSlice"
+
 import { FaMoon } from "react-icons/fa"
 
 export const Header = () => {
   let path = useLocation().pathname
+  const dispatch = useDispatch()
+
   const { currentUser } = useSelector((state) => state.user)
   return (
     <Navbar className="border-b-2">
@@ -39,7 +35,12 @@ export const Header = () => {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
           <FaMoon />
         </Button>
         {currentUser ? (
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import './index.css'
 import { persistor, store } from "./redux/store"
 import { PersistGate } from "redux-persist/integration/react"
 import { Provider } from "react-redux"
+import ThemeProvider from "./components/ThemeProvider"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </PersistGate>
 )
